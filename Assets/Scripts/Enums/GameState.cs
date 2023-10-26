338e7fccd93bcf267be3f70588a2cba73708d429
Shakur Farit <93837330+shakur-farit@users.noreply.github.com>
2023-10-26 17:06:51 +0400

    Create the Game Manager and Room class

---
+++ Assets/Scripts/Enums/GameState.cs
@@ -0,0 +1,15 @@
+public enum GameState
+{
+    none,
+    gameStarted,
+    playingLevel,
+    engagingEnemies,
+    bossStage,
+    engagingBoss,
+    levelCompleted,
+    gameWon,
+    gameLost,
+    gamePaused,
+    dungeonOverviewMap,
+    restartGame
+}
