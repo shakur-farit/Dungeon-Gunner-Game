bb8063ffdcc51d68aa31ed1e2909362bd49b1018
shakur-farit <faritshakur@gmail.com>
2023-11-21 12:03:13 +0400

    Create RoomEnemySpawnParametrs

--- Assets/Scripts/Dungeon/DungeonSO/RoomTemplateSO.cs
+++ Assets/Scripts/Dungeon/DungeonSO/RoomTemplateSO.cs
@@ -24,6 +24,7 @@ public class RoomTemplateSO : ScriptableObject
 
     [Header("Enemy Deatils")]
     public List<SpawnableObjectsByLevel<EnemyDetailsSO>> enemiesByLevelList;
+    public List<RoomEnemySpawnParametrs> roomEnemySpawnParametrsList;
 
     public List<Doorway> GetDoorwayList => doorwayList;
 
@@ -37,8 +38,59 @@ public class RoomTemplateSO : ScriptableObject
             previousPrefab = prefab;
             EditorUtility.SetDirty(this);
         }
+        HelperUtilities.ValidateCheckNullValue(this, nameof(prefab), prefab);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(roomNodeType), roomNodeType);
 
         HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
+
+        if(enemiesByLevelList.Count > 0 || roomEnemySpawnParametrsList.Count > 0)
+        {
+            HelperUtilities.ValidateCheckEnumerableValues(this, nameof(enemiesByLevelList), enemiesByLevelList);
+            HelperUtilities.ValidateCheckEnumerableValues(this, nameof(roomEnemySpawnParametrsList), 
+                roomEnemySpawnParametrsList);
+
+            foreach (RoomEnemySpawnParametrs roomEnemySpawnParametrs in roomEnemySpawnParametrsList)
+            {
+                HelperUtilities.ValidateCheckNullValue(this, nameof(roomEnemySpawnParametrs.DungeonLevel),
+                    roomEnemySpawnParametrs.DungeonLevel);
+                HelperUtilities.ValidateCheckPositiveRange(this, nameof(roomEnemySpawnParametrs.MinTotalEnemiesToSpawn),
+                    roomEnemySpawnParametrs.MinTotalEnemiesToSpawn, nameof(roomEnemySpawnParametrs.MaxTotalEnemiesToSpawn),
+                    roomEnemySpawnParametrs.MaxTotalEnemiesToSpawn, false);
+                HelperUtilities.ValidateCheckPositiveRange(this, nameof(roomEnemySpawnParametrs.MinCocncurrentEnemies),
+                     roomEnemySpawnParametrs.MinCocncurrentEnemies, nameof(roomEnemySpawnParametrs.MaxCocncurrentEnemies),
+                    roomEnemySpawnParametrs.MaxCocncurrentEnemies, false);
+                HelperUtilities.ValidateCheckPositiveRange(this, nameof(roomEnemySpawnParametrs.MinSpawnInterval),
+                    roomEnemySpawnParametrs.MinSpawnInterval, nameof(roomEnemySpawnParametrs.MaxSpawnInterval),
+                    roomEnemySpawnParametrs.MaxSpawnInterval, false);
+
+                bool isEnemyTypeListForDungeonLevel = false;
+                foreach (SpawnableObjectsByLevel<EnemyDetailsSO> dungeonObjectByLevel in enemiesByLevelList)
+                {
+                    if (dungeonObjectByLevel.DungeonLevel == roomEnemySpawnParametrs.DungeonLevel &&
+                        dungeonObjectByLevel.SpawnableObjectRatioList.Count > 0)
+                        isEnemyTypeListForDungeonLevel = true;
+
+                    HelperUtilities.ValidateCheckNullValue(this, nameof(dungeonObjectByLevel.DungeonLevel),
+                        dungeonObjectByLevel.DungeonLevel);
+
+                    foreach (SpawnableObjectRatio<EnemyDetailsSO> dungeonObjectRatio in dungeonObjectByLevel
+                        .SpawnableObjectRatioList)
+                    {
+                        HelperUtilities.ValidateCheckNullValue(this, nameof(dungeonObjectRatio.DungeonObject),
+                            dungeonObjectRatio.DungeonObject);
+
+                        HelperUtilities.ValidateCheckPositiveValue(this, nameof(dungeonObjectRatio.Ratio),
+                            dungeonObjectRatio.Ratio, false);
+                    }
+                }
+
+                if(!isEnemyTypeListForDungeonLevel && roomEnemySpawnParametrs.DungeonLevel != null)
+                {
+                    Debug.Log("No enemy types specified in dor dungeon level " +
+                        roomEnemySpawnParametrs.DungeonLevel.levelName + " in gameobjecy" + this.name.ToString());
+                }
+            }
+        }
 
         HelperUtilities.ValidateCheckEnumerableValues(this, nameof(spawnPositionArray), spawnPositionArray);
     }
---
+++ Assets/Scripts/Dungeon/Room/RoomEnemySpawnParametrs.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class RoomEnemySpawnParametrs
+{
+    public DungeonLevelSO DungeonLevel;
+    public int MinTotalEnemiesToSpawn;
+    public int MaxTotalEnemiesToSpawn;
+    public int MinCocncurrentEnemies;
+    public int MaxCocncurrentEnemies;
+    public int MinSpawnInterval;
+    public int MaxSpawnInterval;
+}
